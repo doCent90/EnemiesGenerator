8ba517c2f75878d9b1c69a3936ce224f0cc90ae1
doCent90 <78255120+doCent90@users.noreply.github.com>
2021-04-14 18:32:23 +0300

    corrected script Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private SpriteRenderer _sprite;
+
     private void Start()
     {
-        
+        _sprite = GetComponent<SpriteRenderer>();
+        SetColor();
+    }
+
+    private void SetColor()
+    {
+        _sprite.color = Color.red;
     }
 }
